165b3fb3d53471e75403857d4a892924bf86bdaa
dima <gldmvl@gmail.com>
2018-10-18 17:43:27 +0300

    Added About page

---
+++ page-about.php
@@ -0,0 +1,239 @@
+<?php get_header() ?>
+<header class="header header-about">
+  <nav class="nav-inverse">
+    <a class="logotype" href="index.html"></a>
+    <ul class="menu">
+      <li><a href="index.html">HOME</a></li>
+      <li><a href="about.html">企業概要</a></li>
+      <li><a href="services.html">サービス</a></li>
+      <li><a href="cases.html">事例紹介</a></li>
+    </ul>
+    <div class="contact-btn-container">
+      <a href="about.html#form">お問い合わせ</a>
+    </div>
+    <div class="close"></div>
+  </nav>
+  <div class="mobile-header-inverse">
+    <h6>BlockTechJapan</h6>
+    <div class="button-nav">
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  </div>
+  <div class="body">
+    <h1>About us</h1>
+    <p>ビジネスを変える、社会を変える、あなたを変える、ブロックチェーンで。</p>
+  </div>
+</header>
+
+<section class="about-section-0">
+  <h3 class="section-title">Company</h3>
+  <div class="section-body">
+    <h3>企業情報</h3>
+    <p>
+      ブロックチェーンテクノロジーが進化するとともに、人間の生活はもっと進化することになります。BlockTechJapanは、2.0、3.0、4.0、次から次へとテクノロジーが革新されてゆく中で、現代社会の抱える欠点が克服され、クリアで誤魔化しの利かないフェアな世界が訪れたときに、皆様のビジネスと共に、新しい世界に残っている企業となれることを目指しています。</p>
+  </div>
+</section>
+
+<section class="about-section-1">
+
+  <div class="section-body">
+    <div class="about-row">
+      <div class="title">会社名</div>
+      <div class="descripton">
+        <div>BlockTechJapan Inc.</div>
+        <div> ブロック・テック・ジャパン</div>
+      </div>
+    </div>
+    <div class="about-row">
+      <div class="title">創業</div>
+      <div class="description">2018年4月15日</div>
+    </div>
+    <div class="about-row">
+      <div class="title">本社所在地</div>
+      <div class="description">米国ニューヨーク</div>
+    </div>
+    <div class="about-row">
+      <div class="title">代表者</div>
+      <div class="description">代表取締役社長 CEO Sasha Kofman (サーシャ・コフマン)</div>
+    </div>
+    <div class="about-row">
+      <div class="title">役員一覧</div>
+      <div class="description">取締役 CIO Maria Mavrekh (マリア・マヴレフ)</div>
+    </div>
+    <div class="about-row">
+      <div class="title">社員数</div>
+      <div class="description">218人</div>
+    </div>
+    <div class="about-row">
+      <div class="title">主な事業</div>
+      <div class="description">
+        <div>インフォメーションテクノロジーに関するセミナー事業</div>
+        <div>ブロックチェーン、AI、IoTをはじめとしたITソリューションの開発事業</div>
+        <div>オフライン・オンラインのによるシステムエンジニア向けの教育事業</div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="about-section-2">
+  <div class="wrapper">
+    <div class="map" id="map">
+
+    </div>
+    <div class="map-note">
+      <h4>BlockTechJapan Inc.</h4>
+      <p>本社所在地</p>
+      <p>350 W 42nd St, New York, NY 10036</p>
+    </div>
+  </div>
+</section>
+
+<section class="about-section-3" id="about-section-3">
+  <h3 class="section-title">Message</h3>
+  <div class="section-body">
+    <h5>ブロックチェーンは、下剋上を可能にする。</h5>
+    <p>
+      ブロックチェーンにより、世界中のあらゆる国において例外なく、すべての産業が変わると言われています。ユーザー同士が繋がるネットワーク上に構築されるブロックチェーンは、改竄される心配がなく、分散型のデータベースとして機能するため、人類のあらゆる営みにおいて大いにその威力を発揮します。創造的破壊をもたらすブロックチェーンは、既存のビジネスモデルへの「下剋上」を可能にします。しかし、それを実行に移すための技術者は世界規模で不足しているため、ほとんどの企業ではブロックチェーンによるイノベーションが起きていないのが現状です。</p>
+    <p>
+      BlockTechJapanは、その設立以前から「ブロックチェーンを、ビジネスの中へ。」というミッションのもとで活動していたメンバーが中心となって創業された会社です。立ち上げメンバーがそれぞれ得意としていたビジネス領域は、そのまま私たちの提供するサービスへと姿を変えながら、今日では当社事業の三本柱となっています。当社の中核メンバーが築き上げたブロックチェーンコミュニティーとの緊密な協力体制は、過去には実現できなかった、ビジネスとコアテクノロジーの融合を具現化しており、ビジネスのリアルな課題に対して、クリエイティブな解決策で応えることを可能にしています。</p>
+    <p>
+      モノづくり大国である日本において、ブロックチェーンを含む情報テクノロジーの展開はゆったりとしたスピードで進んでおり、近年は外国のベンチャー企業が提供するFinTechやシェアエコノミーのサービスの普及が著しく目立っている。私たちは、これに危機感を感じている日本企業が今、最も求めている「スピード」と「開発力」を提供していきたいと考えています。また、安全、安心、効率、平等などの価値を基盤とするブロックチェーンの啓蒙を通じて、日本の各業界で急がれているデジタル・トランスフォーメーションに貢献したいと思います。</p>
+    <p>これからも、皆様のご支援・応援いただけましたら幸いです。</p>
+    <div class="note">
+      <span>BlockTechJapan</span>
+      <span>代表取締役社長 CEO</span>
+      <span>サーシャ・コフマン</span>
+    </div>
+    <div class="man"></div>
+  </div>
+  <div class="bg-1"></div>
+  <div class="bg-2"></div>
+  <div class="bg-3"></div>
+  <div class="bg-4"></div>
+</section>
+
+<section class="about-section-4">
+  <h3 class="section-title">Partners</h3>
+  <div class="section-body">
+    <div class="partner">
+      <div class="description">
+        <h4>DBIC</h4>
+        <h5>デジタルビジネス・イノベーションセンター</h5>
+        <p>
+          DBICは、日本を代表するメンバー企業が集結して2015年6月に設立された。ビジネスにイノベーションを起こすためのプラットフォームとして活動。国内外における様々なトレーニング活動やプロジェクトを通じて、メンバー企業のイノベーション促進を実現しています。</p>
+        <a href="//www.dbic.jp/">サイトを見る</a>
+      </div>
+      <div class="image">
+        <img src="media/about/partner.png" alt="DBIC">
+      </div>
+    </div>
+    <div class="section-end">さらに表示</div>
+  </div>
+</section>
+
+<section class="about-section-5">
+  <div class="wrapper">
+    <img src="media/about/bg2.png" alt="message">
+    <h4>私たちのビジョンとミッション</h4>
+    <p>
+      <span><strong>CEO サーシャ・コフマンが語る、</strong></span>
+      <span>BlockTechJapanが目指しているもの</span>
+    </p>
+    <a class="js-show-interview" href="#">インタビューを読む</a>
+    <span class="arrow-bottom"></span>
+  </div>
+</section>
+
+<div class="about-section-6">
+  <div class="question">― サーシャ氏は過去にヨーロッパや米国でいくつかの事業立ち上げを経験していると聞きます。今回、日本に進出する理由はなんでしょう。</div>
+  <div class="answer">
+    <p>
+      世界中のブロックチェーン事業者と交流していると不思議に思うことがあります。それは、世界有数の経済力を持っている先進国のうち、日本だけがブロックチェーンを積極的にビジネスに取り入れようとしていないからです。そして、その裏には、ビジネスに実装できるほどブロックチェーンを得意とする日本人技術者・有識者が大変不足しているから、という実態も突き止めました。ですから、私たちは世界を代表する経済大国「ニッポン」でありながらも、まだまだブロックチェーンの可能性が開かれていないにジャパン・マーケットに大変大きな関心と期待を寄せています。</p>
+  </div>
+  <div class="question">― それでは、BlockTechJapanの事業概要について教えてください。</div>
+  <div class="answer">
+    <p>
+      当社の事業は、「一貫性」を重要視しています。ですから、ブロックチェーン導入に向けたセミナーからはじまり、その後は、コンサルティングやマーケティング、設計、開発、運用、教育、情報配信を一貫してオールインワンで行える事業体制を整えています。
+      そうすることで、当社は、無駄なく矛盾なく、より効率的にブロックチェーンをお客様の組織内に導入していくことを実現します。</p>
+  </div>
+  <div class="question">― ブロックチェーン開発のサービスについて詳しく教えてください。</div>
+  <div class="answer">
+    <p>
+      ブロックチェーンの導入は、各企業が目指す目標に合わせてカスタマイズ設計されるべきです。例えば、給与システムだけにブロックチェーンを導入したり、営業チームの管理だけにブロックチェーンを導入したり、組織内バラバラとした導入では十分な効果を期待できません。そのため、ブロックチェーンをトータルに実装できる設計力・開発力こそが当社の最大の強みです。プロジェクトはエンジニアの設計能力が高いほど有意義に進みます。もちろん案件の規模にもよりますが、通常のケースでは、1チーム12名体制で開発を行います。大規模なプロジェクトの場合には、お客様とも相談しながら、プロジェクトに参加する開発チームの数を増やしていきます。設計期間を除いた開発期間は、短いもので3か月、通常は半年~1年です。</p>
+  </div>
+  <div class="question">― 自社製品ではなく、ビジネス向けのブロックチェーン開発のサポートにフォーカスした理由はなんでしょう。</div>
+  <div class="answer">
+    <p>
+      既存のマーケットに挑むベンチャー企業達は、様々な自社製品やサービスの開発を行っています。このような新しい組織では、プロダクトの開発スピードが大手企業の開発スピードを遥かに上回ります。しかし、ベンチャー企業の多くは未熟な組織体制と限られた資金リソースゆえに、世の中を変えることができる製品を完成させることがないまま、ビジネスの表舞台から退場することが多いです。</p>
+    <p>
+      そうした事実から、私たちは、世の中のために役立つ製品またはサービスの提供に携わりたいという思いから、すでにビジネスに長けた企業向けのブロックチェーン開発を行うことに特化し、大手企業でもスピーディーなブロックチェーン開発を実現できるようなサービスの提供に専念することにしています。</p>
+    <p>
+      また、これまでは「大企業＝信用＝高セキュリティ」という信用の構造があり、大手企業は大手企業にシステム開発の依頼をする傾向があったわけですが、ブロックチェーン開発の場合は、分散型ネットワークという性質もあり、私たちのような「開発者集団企業」が作ったブロックチェーンでも高い信頼性やセキュリティを確保できるためため、開発コストの安さやスピードの速さを重視して、案件を依頼してくれるクライアント様が増えているのを肌で感じるほどマーケットが変わってきています。</p>
+  </div>
+  <div class="question">―具体的にはどのような業界にサービスを提供したいと考えていますか。</div>
+  <div class="answer">
+    <p>世界各国、将来的には例外なくすべての産業分野にブロックチェーンが導入されていきます。インターネット・デバイス・IoTに関するテクノロジーの進化とともに、ブロックチェーンの需要はさらに高まります。
+      実質改竄不可能な利用者同士の信用取引構造は、世界中の大半の人に便利で理想的だからです。したがって、我々が業界を選ぶという感覚はなく、どの業界が先に我々を選んでくれたかが重要になります。「世界を変えたい」という強いミッション感を持ち、ブロックチェーン導入のスピードを重視する組織にサービスを提供したいと考えています。</p>
+  </div>
+  <div class="question">―日本とのビジネスにおいて一番難しいところを教えてください。</div>
+  <div class="answer">
+    <p>
+      日本人は、独特の商習慣があることで有名です。世界中の誰もが、日本でビジネスを行う難易度は高いと思っています。しかし、日本とのビジネスに成功し、軌道に乗せた企業は、その後世界各国でも成功し、長寿ビジネスになるという例も良く聞きます。</p>
+    <p>慎重で用心深い日本人だからこそ、信用を得るまでのプロセスが最も難しく重要かと思っていますので、ブロックチェーンという新しい信用の築き方を通じ、積極的に日本人とのビジネスを進めていきたいと考えています。</p>
+  </div>
+  <div class="question">― 日本にオフィスをオープンする予定はありますか。</div>
+  <div class="answer">
+    <p>はい。今後の利便性を上げるために必要であれば、すぐにでも東京オフィスをオープンしたいと考えています。</p>
+  </div>
+  <div class="question">―今後の企業成長のために、どんなビジョンを描いていますか。</div>
+  <div class="answer">
+    <p>
+      ほんの２０年前には小さなベンチャー企業でしかなかったいくつかの会社は、新しいツール「インターネット」を巧みに活用し、その企業サイズを怪物的なものに変え、世界に君臨しているのはなぜでしょうか。もともとは世界中みんなのために作られたインターネットは、このような企業に支配され、他者を寄せ付けませんでした。ブロックチェーンの登場により、少しずつではありますが「非中央集権」の流れができています。私たちは、これを加速させるべく、一強を目指さず、役割分担し、より多くの企業がブロックチェーンの利便性を享受できるようなサービスを幅広く提供していきたいと考えています。</p>
+  </div>
+</div>
+
+<section class="about-section-7" id="form">
+  <div class="form">
+    <h3 class="section-title">Contact</h3>
+    <form>
+      <div class="fsection-form-row">
+        <div class="form-column">
+          <label for="input-1" class="label-required">お名前（ご担当者様名）</label>
+          <input type="text" id="input-1">
+        </div>
+      </div>
+      <div class="section-form-row">
+        <div class="form-column">
+          <label for="input-2" class="label-required">会社名</label>
+          <input type="text" id="input-2">
+        </div>
+        <div class="form-column">
+          <label for="input-3">部署名</label>
+          <input type="text" id="input-3">
+        </div>
+      </div>
+      <div class="section-form-row">
+        <div class="form-column">
+          <label for="input-4" class="label-required">メールアドレス</label>
+          <input type="text" id="input-4">
+        </div>
+        <div class="form-column">
+          <label for="input-5" class="label-required">お電話番号</label>
+          <input type="text" id="input-5">
+        </div>
+      </div>
+      <div class="section-form-row">
+        <div class="form-column">
+          <label for="input-6">こちらにお問い合わせ内容をご記入ください。</label>
+          <textarea id="input-6"></textarea>
+        </div>
+      </div>
+      <div class="section-form-row">
+        <input type="submit" value="送信">
+      </div>
+    </form>
+  </div>
+</section>
+<?php get_footer() ?>
